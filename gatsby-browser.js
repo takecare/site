3e2aeb0242261c2d07254b67aff945e263522fdc
takecare <rui@vazteixeira.org>
2020-04-24 18:04:57 +0100

    remove global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,10 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-
-/*
-In most cases, the best way to add global styles is with a shared layout
-component. Check out the docs for more on that approach.
-*/
-
-import "./src/styles/global.css"
