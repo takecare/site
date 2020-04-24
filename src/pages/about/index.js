a0a1ae4f951e5f8769eda51a814718b6cee923b1
takecare <rui@vazteixeira.org>
2020-04-24 18:02:07 +0100

    rename page-2 to about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
 
-const SecondPage = () => (
+import styles from "./styles.module.css"
+
+const About = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="About" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default About
