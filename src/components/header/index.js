068f1a37afc0f3234f212ae1cbd429b3592e0a13
takecare <rui@vazteixeira.org>
2020-06-06 18:29:58 +0100

    cleanup basic styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,16 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
 
 import styles from "./styles.module.css"
 
-const Header = ({ title }) => (
-  <header className={styles.header}>
+const Header = ({ title, description }) => (
+  <header>
     <div>
-      <h1>
+      <h1 className={styles.subheader}>
         <Link to="/">{title}</Link>
       </h1>
+      <h6>{description}</h6>
     </div>
   </header>
 )
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -7,15 +7,14 @@ import Menu from "../menu"
 import Header from "../header"
 import Footer from "../footer"
 
-// import styles from "./styles.module.css"
+import styles from "./styles.module.css"
 
 const Layout = ({ children }) => {
-  // https://www.gatsbyjs.org/docs/use-static-query/
-  const { title } = useSiteMetadata()
+  const { title, description } = useSiteMetadata()
 
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
-      <Header title={title} />
+    <div className={styles.layout}>
+      <Header title={title} description={description} />
       <Menu />
       <div>
         <main style={{}}>{children}</main>
